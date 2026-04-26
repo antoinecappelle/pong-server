cc1f493968d12e35208927b0562ad1d91a1212de
fourmi <antoine.ac.cappelle@gmail.com>
2026-04-26 13:54:00 +0200

    Refactor server.js to use Express and update player roles

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-const io = require('socket.io')(process.env.PORT || 3000, {
-    cors: { origin: "*" }
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" } // Très important pour autoriser Discord/GitHub Pages
 });
 
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Un joueur s\'est connecté : ' + socket.id);
-    
-    // Assigne le joueur 1 ou 2
-    if (Object.keys(players).length < 2) {
-        players[socket.id] = { x: 0, y: 160, id: socket.id };
+    console.log('Connecté:', socket.id);
+
+    // Attribution des rôles
+    if (Object.keys(players).length === 0) {
+        players[socket.id] = { side: 'left' };
+        socket.emit('role', 'left');
+    } else if (Object.keys(players).length === 1) {
+        players[socket.id] = { side: 'right' };
+        socket.emit('role', 'right');
+    } else {
+        socket.emit('error', 'Partie pleine');
     }
 
-    socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].y = data.y;
-            socket.broadcast.emit('update', { id: socket.id, y: data.y });
-        }
+    // Relais des mouvements de raquette
+    socket.on('move', (y) => {
+        socket.broadcast.emit('opponentMove', y);
+    });
+
+    // Relais de la position de la balle (calculée par le joueur de gauche)
+    socket.on('ballSync', (ballData) => {
+        socket.broadcast.emit('ballUpdate', ballData);
     });
 
     socket.on('disconnect', () => {
         delete players[socket.id];
     });
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Serveur prêt sur le port ${PORT}`));
